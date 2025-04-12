c73a06160b6885534c29eccde0008ce13d88d06f
IKRAMELHADI <152861932+IKRAMELHADI@users.noreply.github.com>
2025-04-12 17:16:09 +0200

    Connexion Mongo&node

--- Backend/index.js
+++ Backend/index.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const mongoose= require('mongoose');
 const app = express();
-const port = 3000;
+const connectDB =async()=>{
+  mongoose.connect("mongodb://localhost:27017/e-comm");
+  const productSchema=new mongoose.Schema({});
+  const product=mongoose.model("products",productSchema);
+  const data = await product.find( );
+  console.warn(data);
+}
 
-app.get('/', (req, res) => {
-  res.send('Bonjour depuis Docker !');
-});
+connectDB();
+const port = 5000;
+
+
 
 app.listen(port, () => {
   console.log(`L'application écoute sur le port ${port}`);
