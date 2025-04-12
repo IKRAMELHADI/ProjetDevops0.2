e5472d9c52e8be6bdf12a5b038aeae606de6c4b3
IKRAMELHADI <152861932+IKRAMELHADI@users.noreply.github.com>
2025-04-12 22:08:56 +0200

    Ajout des dockerfiles

--- Backend/index.js
+++ Backend/index.js
@@ -5,14 +5,20 @@ const User = require("./db/User");
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",  // Autorise toutes les origines pour les tests, sinon tu peux spécifier ton origine ici
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"], 
+  credentials: true
+}));
 
 
 app.post("/register",async(req,resp)=>{
-  let user = new Use (req.body);
+  let user = new User (req.body);
   let result = await user.save();
   resp.send(result)
 })
+
 
 
 const port = 5000;
@@ -22,3 +28,6 @@ const port = 5000;
 app.listen(port, () => {
   console.log(`L'application écoute sur le port ${port}`);
 });
+
+
+
