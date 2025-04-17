5ec8cf841224529a51c6479f2746a36010e08113
IKRAMELHADI <152861932+IKRAMELHADI@users.noreply.github.com>
2025-04-17 20:23:01 +0200

    Ajout Login Lougout & intégration Api

--- Backend/index.js
+++ Backend/index.js
@@ -13,15 +13,30 @@ app.use(cors({
 }));
 
 
-app.post("/register",async(req,resp)=>{
+app.post('/register',async(req,resp)=>{
   let user = new User (req.body);
   let result = await user.save();
+  result = result.toObject();
+  delete result.password;
   resp.send(result)
 })
 
+app.post('/login',async(req,resp)=>{
+  if(req.body.password && req.body.email){
+  let user = await User.findOne(req.body).select("-password");
+  if(user)
+  {
+    resp.send(user);
+  }else{
+    resp.send({result:" No User found"});
+  }
+  
+}else{
+  resp.send({result:" No User found"});
+}
+})
 
-
-const port = 5000;
+const port =8000;
 
 
 
